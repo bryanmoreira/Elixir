76251bd887b3c79607c3e3eaff73dde1c5daae0f
Bryan Moreira <49196485+bryanmoreira@users.noreply.github.com>
2021-12-15 21:38:43 -0300

    1.1

--- teste.exs
+++ teste.exs
@@ -1,7 +1,22 @@
 defmodule MeuModulo do
-    import IO
+    import IO, only: [puts: 1]
+    import Kernel, except: [inspect: 1]
+
+    alias MeuModulo.Math, as: MyMath
+
+    require Integer
 
     def ola_mundo do
-        puts("Ola")
+        inspect(MyMath.soma(2, 2))
+    end
+
+    def exibe_se_eh_par(numero) do
+        puts("O numero #{numero} eh par? #{Integer.is_even(numero)}")
+    end
+
+    def inspect(parametro) do
+        puts("Comecando a inspecao")
+        puts(parametro)
+        puts("Terminando a inspecao")
     end
 end
